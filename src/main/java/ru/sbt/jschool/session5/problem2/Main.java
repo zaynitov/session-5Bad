ff26a8e06410f70fdde918ee7cad6f0d3dc2aec7
zaynitov <zaynitov1@yandex.ru>
2018-04-06 14:31:53 +0300

    2st

--- src/main/java/ru/sbt/jschool/session5/problem2/Main.java
+++ src/main/java/ru/sbt/jschool/session5/problem2/Main.java
@@ -13,12 +13,10 @@ public class Main {
 
         StringBuilder resultString = new StringBuilder("{");
         Class className = someObject.getClass();
+        JSONHelper jsonHelper = new JSONHelper();
 
-        if (className.isArray()||className == Double.class || className == Float.class || className == Long.class ||
-                className == Integer.class || className == Short.class || className == Character.class ||
-                className == Byte.class || className == Boolean.class || className == Set.class || className == List.class ||
-                className == Date.class || className == Calendar.class || className == String.class) {
-            resultString.append(toJSONObj("", someObject));
+        if (jsonHelper.classesWeCannSupport.contains(className)||className.isArray()) {
+            resultString.append(jsonHelper.toJSON("", someObject));
 
             resultString.delete(0,4);       //delete smth what we don't need to make the same as Jackson
             return resultString.delete(resultString.length()-1,resultString.length()).toString();
@@ -29,7 +27,7 @@ public class Main {
                         field.setAccessible(true);
                         Object value = field.get(someObject);
                         if (value != null) {
-                            resultString.append(toJSONObj(field.getName(), value));
+                            resultString.append(jsonHelper.toJSON(field.getName(), value));
                         }
                     }
                     className = className.getSuperclass();
@@ -46,76 +44,6 @@ public class Main {
             resultString.append("}");
         }
         return resultString.toString();
-    }
-
-
-    public String toJSONObj(String fieldName, Object field) {
-        StringBuilder resultTermString = new StringBuilder();
-
-        if ((field instanceof List)) {
-            Object[] objects = ((List) field).toArray();
-            return arrayToString(fieldName, objects).toString();
-
-        }
-        if ((field instanceof Set)) {
-            Object[] objects = ((Set) field).toArray();
-            return arrayToString(fieldName, objects);
-
-
-        }
-        if (field.getClass().isArray()) {
-            return arrayToString(fieldName, (Object[]) field);
-        }
-        if ((field instanceof Date)) {
-            resultTermString.append("\"" + fieldName + "\":");
-            resultTermString.append("\"" + new SimpleDateFormat("dd.MM.yyyy").format(field) + "\"" + ",");
-            return resultTermString.toString();
-
-
-        }
-        if ((field instanceof Calendar)) {
-            Date date = ((Calendar) field).getTime();
-            resultTermString.append("\"" + fieldName + "\":");
-            resultTermString.append("\"" + new SimpleDateFormat("dd.MM.yyyy").format(date) + "\"" + ",");
-            return resultTermString.toString();
-        }
-
-
-        Class type = field.getClass();
-        //	isPrimitiveWrapper(Class<?> type) from Apache.smth.* doesn't work(don't know why(i used dependencies)) So:
-        if (type == Double.class || type == Float.class || type == Long.class ||
-                type == Integer.class || type == Short.class || type == Character.class ||
-                type == Byte.class || type == Boolean.class) {
-            return ("\"" + fieldName + "\":" + field.toString() + ",");
-        }
-        if (type == String.class) return ("\"" + fieldName + "\":" + "\"" + field.toString() + "\"" + ",");
-
-
-        return newClasstoJSON(fieldName, field); //Here we put new Logic for formatting new Classes
-
-    }
-
-    public String arrayToString(String fieldName, Object[] arrayToString) {
-        StringBuilder resultTermString = new StringBuilder();
-
-
-        resultTermString.append("\"" + fieldName + "\"" + ":[");
-
-        for (int i = 0; i < arrayToString.length; i++) {
-            if (i == (arrayToString.length - 1)) {
-                resultTermString.append("\"" + arrayToString[i].toString() + "\"]");
-                break;
-            }
-            resultTermString.append("\"" + arrayToString[i].toString() + "\",");
-
-        }
-        resultTermString.append(",");
-        return resultTermString.toString();
-    }
-
-    public String newClasstoJSON(String fieldName, Object field) {
-//TODO
-        return "";
     }
 
 }
